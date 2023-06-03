778b61a52eca0d2585785947c7a94ee92be5a496
yuna <cyunanne@gmail.com>
2023-06-03 20:06:06 +0900

    좋아요 버튼 기능 공통코드로 이동, [카테고리 상품목록] css 수정

--- resources/js/common/default.js
+++ resources/js/common/default.js
@@ -20,3 +20,14 @@
         }
     }
 })();
+
+// 좋아요(♡) 클릭 시 이미지 변경
+const likeImages = document.querySelectorAll('img.like');
+for(let like of likeImages) {
+    like.addEventListener('click', () => {
+        if( like.getAttribute('src').includes('fill') )
+            like.setAttribute('src', '/resources/images/common/like-grey.svg');
+        else
+            like.setAttribute('src', '/resources/images/common/like-fill.svg');
+    });
+}
