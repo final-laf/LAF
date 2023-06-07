de56e19baa9e3bff9eb3ada6d96398da02b295d3
yuna <cyunanne@gmail.com>
2023-06-07 15:51:04 +0900

    spring 프로젝트 병합

---
+++ src/main/java/edu/kh/laf/member/model/dto/Member.java
@@ -0,0 +1,11 @@
+package edu.kh.laf.member.model.dto;
+
+import lombok.*;
+
+@Data
+public class Member {
+    private Long memberNo;
+    private String memberId;
+    private String memberPw;
+    private String memberName;
+}
