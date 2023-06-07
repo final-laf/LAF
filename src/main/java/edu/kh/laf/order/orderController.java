c0b3a0b90660386315da7e12ec293f842c8ee962
장반석 <peter920107@gmail.com>
2023-06-07 16:57:41 +0900

    mypage/order_controller연결

---
+++ src/main/java/edu/kh/laf/order/orderController.java
@@ -0,0 +1,13 @@
+package edu.kh.laf.order;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class orderController {
+
+	@GetMapping("/orderdetail")
+	public String orderDetail() {
+		return "/order/orderDetail";
+	}
+}
