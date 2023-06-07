89ab9603e168acc66b66acbc89455d7bd897fb88
woongdam777 <woongdam777@gmail.com>
2023-06-07 17:48:07 +0900

    관리자 파일명 수정 및 페이지연결

--- src/main/java/edu/kh/laf/admin/controller/AdminController.java
+++ src/main/java/edu/kh/laf/admin/controller/AdminController.java
@@ -2,12 +2,39 @@ package edu.kh.laf.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/admin")
 @Controller
 public class AdminController {
 
-	@GetMapping("/admin")
+	@GetMapping("/dashboard")
 	public String adminDashboard() {
 		return "/admin/dashboard";
 	}
+	
+	@GetMapping("/mainview")
+	public String adminMainView() {
+		return "/admin/mainview";
+	}
+	
+	@GetMapping("/member")
+	public String adminMember() {
+		return "/admin/member";
+	}
+	
+	@GetMapping("/productenroll")
+	public String adminProductEnroll() {
+		return "/admin/productenroll";
+	}
+	
+	@GetMapping("/ordercurrent")
+	public String adminOrderCurrent() {
+		return "/admin/ordercurrent";
+	}
+	
+
+	
+	
+	
 }
