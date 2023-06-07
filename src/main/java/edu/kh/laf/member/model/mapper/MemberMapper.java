f9a89b8d78e12a979060e248127a329a2bde1d17
woongdam777 <woongdam777@gmail.com>
2023-06-07 17:04:43 +0900

    다시

--- src/main/java/edu/kh/laf/member/model/mapper/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package edu.kh.laf.member.model.mapper;
-
-import edu.kh.laf.member.model.dto.Member;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MemberMapper {
-    Member selectMember(long id);
-}
