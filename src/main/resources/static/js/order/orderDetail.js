878258b71989bdb01abf810002c7d75610838915
DESKTOP-QES8KP2\peter <peter9217@naver.com>
2023-06-08 23:36:23 +0900

    쿠폰 모달창 완료

--- src/main/resources/static/js/order/orderDetail.js
+++ src/main/resources/static/js/order/orderDetail.js
@@ -7,3 +7,17 @@ document.getElementById("orderShippingBack").addEventListener("click", () => {
     document.getElementById("orderShippingBack").style.display="none";
     document.getElementById("orderShippingContent").style.display="none";
 })
+
+document.getElementById("orderDetailCoupon").addEventListener("click", () => {
+    document.getElementById("orderCouponBack").style.display="block";
+    document.getElementById("orderCouponContent").style.display="block";
+})
+
+document.getElementById("orderCouponBack").addEventListener("click", () => {
+    document.getElementById("orderCouponBack").style.display="none";
+    document.getElementById("orderCouponContent").style.display="none";
+})
+document.getElementById("orderCouponCancel").addEventListener("click", () => {
+    document.getElementById("orderCouponBack").style.display="none";
+    document.getElementById("orderCouponContent").style.display="none";
+})
