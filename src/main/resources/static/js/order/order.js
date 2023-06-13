80668cd5f9f59d240736f0d58d24c9c14d847398
woongdam777 <woongdam777@gmail.com>
2023-06-13 17:48:46 +0900

    배송지 화면 통합, 모달창 수정, 주소 세팅,결제화면 할인금액, 결제금액 세팅

--- src/main/resources/static/js/order/order.js
+++ src/main/resources/static/js/order/order.js
@@ -28,3 +28,39 @@ document.getElementById("orderCouponCancel").addEventListener("click", () => {
   document.body.style.removeProperty('overflow');
 })
 
+// 상품 가격 계산
+const table = document.getElementById("mypageOrderProduct");
+let totalAmount = 0; // 총상품금액
+let totalDiscount = 0; // 총할인금액
+
+for (let i = 1; i < table.rows.length - 1; i++) {
+  let row = table.rows[i];
+  
+  let quantity = parseInt(row.cells[3].innerText);
+  let price = parseInt(row.cells[4].innerText.replace(",", ""));
+  
+  let rowAmount = quantity * price;
+
+  totalAmount += rowAmount; // 총상품금액
+
+  if (row.cells[5] != null) {
+    let discountAmount = parseInt(row.cells[5].innerText.replace(",", ""));
+    totalDiscount += discountAmount; // 총할인금액
+  }
+}
+
+// 상품 총 할인 금액 세팅
+document.getElementById("totalDiscount").innerText = totalDiscount.toLocaleString() + '원';
+// 주문상품 총 금액 세팅
+document.getElementById("totalAmount").innerText = totalAmount.toLocaleString() + '원';
+
+
+// 할인적용금액 세팅
+const td = document.getElementById("applyDiscount").innerText;
+document.getElementById("payDiscount").innerText = td;
+
+
+// 최종결제금액
+const payment = (totalAmount + totalDiscount + 2500).toLocaleString();
+document.getElementById("payment").innerText = payment + '원';
+document.getElementById("paymentBtn").innerText = payment + '원 결제하기';
